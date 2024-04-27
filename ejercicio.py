eed3bfc1e40d99fdbd44c032da9d20352bf9e0f4
Kevin Jacome <kjacomeg@est.ups.edu.ec>
2024-04-26 19:56:55 -0500

    Resolucion de ejercicios

--- ejercicio.py
+++ ejercicio.py
@@ -7,13 +7,12 @@
 # Imprime el valor asociado a la clave "banana" en la consola.
 # Imprime el valor asociado a la clave "uva" en la consola.
 
-frutas = {
-    "manzana": "roja",
-    "banana": , # banana lleva el valor "amarilla"
+frutas = dict ({
+    "manzana":"roja",
+    "banana": "amarilla", # banana lleva el valor "amarilla"
     "pera": "verde",
-    "naranja": # naranja lleva el valor "naranja"
-}
+    "naranja": "naranja"}) # naranja lleva el valor "naranja"
 
 print(frutas)
-print() # banana lleva el valor "amarilla"
+print(frutas.get("banana")) # banana lleva el valor "amarilla"
 print(frutas.get("uva"))
